dc3888c710df441c9903e56f7ad2c8174c396421
Bintang Immanuela <bintangimmanuela@gmail.com>
2024-06-27 01:58:34 +0700

    Init

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,6 @@
+from flask import Flask, request, render_template
+from TM.TMPengurangan import TMPengurangan
+from TM.TMFaktorial import TMFaktorial
+from TM.TMPembagian import TMPembagian
+
+app = Flask(__name__)
